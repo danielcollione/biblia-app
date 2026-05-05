c91f48eb9881f4d4b41a32726c108d17ba0a03b7
daniel.christino@sp01.local <daniel.collione22@gmail.com>
2026-05-05 14:43:22 -0300

    feat: ajuste

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'home/library/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
